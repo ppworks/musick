b0c382751ccda437ef823698f5a870eacda893ee
koshikawa <koshikawa@ppworks.jp>
2011-09-04 00:37:12 +0900

    artist, artist_alias seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+unless Rails.env.test?
+  # artists
+  if Artist.all.count == 0
+    artists = File.read("#{Rails.root}/db/seeds/artists.yml")
+    Artist.delete_all
+    artist_list = YAML.load(artists).symbolize_keys
+    artist_list[:artists].each do |a|
+      Artist.find_or_create_by_name(a)
+    end
+  end
+  
+  # artist_aliases
+  if ArtistAlias.all.count == 0
+    artist_aliases = File.read("#{Rails.root}/db/seeds/artist_aliases.yml")
+    ArtistAlias.delete_all
+    artist_aliases_list = YAML.load(artist_aliases).symbolize_keys
+    artist_aliases_list[:artist_aliases].each do |key, val|
+      artist = Artist.find_by_name key
+      if artist.present?
+        ArtistAlias.create(:artist_id => artist.id, :name => val)
+      end
+    end
+  end
+end
