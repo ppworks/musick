e8ff658b8e16055c7e9dbfb9407677b89cd4acec
koshikawa <koshikawa@ppworks.jp>
2011-09-03 22:08:50 +0900

    rails new musick -T -J -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musick::Application.config.secret_token = '4f7c9171a3c2096e24623c5ac93e2b3c2712452b74dbf6910361acc97b3f86c88d7ac347d61057c62099dc820c4c9cbba6639e7b15527e93335691bf039e60a8'
