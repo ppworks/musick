1c50b9f56535ef68a84f5012b2517fe851d34e58
koshikawa <koshikawa@ppworks.jp>
2011-09-30 21:56:03 +0900

    rspec 書いた controllers/artists_controller_spec.rb

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -9,4 +9,29 @@ describe ArtistsController do
     end
   end
 
+  describe "GET 'show'" do
+    it "should be successful" do
+      artist = Factory.create :artist_larc
+      Artist.stub(:find_and_update_lastfm) {artist}
+      get 'show', :id => 1
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'search'" do
+    it "should be successful" do
+      artist = Factory.create :artist_larc
+      get 'search', :keyword => "L'Arc~en~Ciel"
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'search_lastfm'" do
+    it "should be successful" do
+      artist = Factory.create :artist_larc
+      Artist.stub(:create_by_lastfm) {[artist]}
+      get 'search_lastfm', :keyword => "L'Arc~en~Ciel", :format => :js
+      response.should be_success
+    end
+  end
 end
--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -1,6 +1,11 @@
 # coding: utf-8
 FactoryGirl.define do
   factory :artist_larc, :class => Artist do
-    name "L'Arc〜en〜Ciel"
+    id 1
+    name "L'Arc~en~Ciel"
+    show_flg TRUE
+    created_at Time.now
+    updated_at Time.now
+    image_searched_at Time.now
   end
 end
