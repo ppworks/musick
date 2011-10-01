eac31a4243eb7e49ebcdd0d587f44488723f399a
koshikawa <koshikawa@ppworks.jp>
2011-10-01 23:47:00 +0900

    amazonカートに入れる実装

--- config/initializers/amazon_ecs.rb
+++ config/initializers/amazon_ecs.rb
@@ -1,7 +1,7 @@
 require 'amazon/ecs'
 Amazon::Ecs.debug = true
 Amazon::Ecs.options = {
-  :associate_tag => 'musick0f-22',
+  :associate_tag => APP_CONFIG[:amazon_associate_tag],
   :AWS_access_key_id => ENV['AMAZON_KEY'],
   :AWS_secret_key => ENV['AMAZON_SECRET'],
   :country => :jp
