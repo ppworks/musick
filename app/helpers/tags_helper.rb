70f965c93fc61a603cb0134fa1fc55ab0be639fa
koshikawa <koshikawa@ppworks.jp>
2011-10-22 09:40:34 +0900

    tag付け artist_item, artist_track対応

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -23,4 +23,30 @@ module TagsHelper
       link_to t('musick.users_artists_tag.create', :tag => tag_name), login_path, :class => 'button popup'
     end
   end
+  
+  def link_to_users_artist_items_tags_create_or_destroy artist_id, item_asin, tag_id, tag_name
+    if user_signed_in?
+      artist_item = ArtistItem.find_by_artist_id_and_asin artist_id, item_asin
+      if artist_item.present? && current_user.users_artist_items_tags.where(:artist_item_id => artist_item.id).where(:tag_id => tag_id).exists?
+        link_to t('musick.users_artist_items_tag.destroy', :tag => tag_name), users_destroy_artist_items_tag_path(artist_id, item_asin, tag_id), :class => 'button destroy', :method => :delete, :remote => true
+      else
+        link_to t('musick.users_artist_items_tag.create', :tag => tag_name), users_create_artist_items_tag_path(artist_id, item_asin, tag_id), :class => 'button create', :method => :post, :remote => true
+      end
+    else
+      link_to t('musick.users_artist_items_tag.create', :tag => tag_name), login_path, :class => 'button popup'
+    end
+  end
+  
+  def link_to_users_artist_tracks_tags_create_or_destroy artist_id, item_asin, disc, track, tag_id, tag_name
+    if user_signed_in?
+      artist_track = ArtistTrack.find_by_artist_id_and_asin_and_disc_and_track artist_id, item_asin, disc, track
+      if artist_track.present? && current_user.users_artist_tracks_tags.where(:artist_track_id => artist_track.id).where(:tag_id => tag_id).exists?
+        link_to t('musick.users_artist_tracks_tag.destroy', :tag => tag_name), users_destroy_artist_tracks_tag_path(artist_id, item_asin, disc, track, tag_id), :class => 'button destroy', :method => :delete, :remote => true
+      else
+        link_to t('musick.users_artist_tracks_tag.create', :tag => tag_name), users_create_artist_tracks_tag_path(artist_id, item_asin, disc, track, tag_id), :class => 'button create', :method => :post, :remote => true
+      end
+    else
+      link_to t('musick.users_artist_tracks_tag.create', :tag => tag_name), login_path, :class => 'button popup'
+    end
+  end
 end
